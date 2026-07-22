8169c1954df20c1ce8f41d88883c4f2ed53b5de3
mostafa <mr6928387@gmail.com>
2026-07-22 17:18:39 +0300

    Solve LeetCode 142: Linked List Cycle II

---
+++ Medium/Linked List/0142_Linked_List_Cycle_II.py
@@ -0,0 +1,41 @@
+"""
+LeetCode 142 - Linked List Cycle II
+
+Difficulty: Medium
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Technique:
+- Fast & Slow Pointers (Floyd's Cycle Detection)
+"""
+
+
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow is fast:
+                slow = head
+
+                while slow is not fast:
+                    slow = slow.next
+                    fast = fast.next
+
+                return slow
+
+        return None
