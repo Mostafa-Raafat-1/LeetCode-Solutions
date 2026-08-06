59f69a09bf773788767c788ffcbe1e8b4219d80d
mostafa <mr6928387@gmail.com>
2026-08-07 01:58:56 +0300

    Solve LeetCode 344: Reverse String (using recursion)

--- Easy/Strings/0344_Reverse_String.py
+++ Easy/Strings/0344_Reverse_String.py
@@ -10,16 +10,43 @@ Technique:
 - Two Pointers
 """
 
+"""---------------- Two Pointers ----------------"""
+# from typing import List
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         left = 0
+#         right = len(s) - 1
+
+#         while right > left:
+#             s[left], s[right] = s[right], s[left]
+#             left +=1
+#             right -=1
+
+
+"""---------------- Recursion ----------------"""
+"""
+LeetCode 344 - Reverse String
+
+Difficulty: Easy
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+Technique:
+- Recursion
+- Two Pointers (Recursive)
+"""
 
 from typing import List
 
 
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        left = 0
-        right = len(s) - 1
-        
-        while right > left:
+        def swap(left, right):
+            if left >= right:
+                return
             s[left], s[right] = s[right], s[left]
-            left +=1
-            right -=1
+
+            swap(left + 1, right - 1)
+
+        swap(0, len(s) - 1)
