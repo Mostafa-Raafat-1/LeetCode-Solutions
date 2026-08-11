df14137eabd2b22687ab0a8673f755dab1f7c228
mostafa <mr6928387@gmail.com>
2026-08-11 18:42:31 +0300

    Solve LeetCode 21: Merge Two Sorted Lists (Recursively)

--- Easy/Linked List/0021_Merge_Two_Sorted_Lists.py
+++ Easy/Linked List/0021_Merge_Two_Sorted_Lists.py
@@ -10,11 +10,55 @@ Technique:
 - Dummy Node
 """
 
+"""-------------- Iterative --------------"""
+# from typing import Optional
 
+
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+# class Solution:
+#     def mergeTwoLists(
+#         self,
+#         list1: Optional[ListNode],
+#         list2: Optional[ListNode],
+#     ) -> Optional[ListNode]:
+#         dummy = ListNode()
+#         tail = dummy
+
+#         while list1 and list2:
+#             if list1.val <= list2.val:
+#                 tail.next = list1
+#                 list1 = list1.next
+#             else:
+#                 tail.next = list2
+#                 list2 = list2.next
+
+#             tail = tail.next
+
+#         tail.next = list1 if list1 else list2
+
+#         return dummy.next
+
+
+"""
+LeetCode 21 - Merge Two Sorted Lists
+
+Difficulty: Easy
+
+Time Complexity: O(n + m)
+Space Complexity: O(n + m)
+
+Technique:
+- Recursion
+"""
+"""-------------- Recursive --------------"""
 from typing import Optional
 
 
-# Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -27,19 +71,12 @@ class Solution:
         list1: Optional[ListNode],
         list2: Optional[ListNode],
     ) -> Optional[ListNode]:
-        dummy = ListNode()
-        tail = dummy
+        if not list1 or not list2:
+            return list1 or list2
 
-        while list1 and list2:
-            if list1.val <= list2.val:
-                tail.next = list1
-                list1 = list1.next
-            else:
-                tail.next = list2
-                list2 = list2.next
-
-            tail = tail.next
-
-        tail.next = list1 if list1 else list2
-
-        return dummy.next
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
